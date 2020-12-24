c92176586ae8673efc96e70cc89ad3267b1c775f
Sunny20001114 <75824372+Sunny20001114@users.noreply.github.com>
2020-12-24 12:32:24 +0800

    Update Page0-2.py
    許庭瑄加油

--- Page0-2.py
+++ Page0-2.py
@@ -65,40 +65,48 @@ class PageA:
         self.window = tk.Toplevel()
         self.window.geometry('600x420')
         self.window.title('會議時間')
+        self.text = tk.StringVar()  # 新增
+        self.text.set("")  # 新增
         # self.lblTitle_B = tk.Label(self.pageB, text = "創建新會議", height = 1 , width = 15, font = f1, bg = 'DarkSlateGray', fg = 'white', anchor = 'w')
         self.lblname = tk.Label(self.window, text = "會議名稱：", height = 1, width = 10, font = f2)
+        self.lblname1 = tk.Label(self.window, text = "你已選擇：",font = f2)  # 新增
         
         global meeting_name
         
         meeting_name = tk.StringVar()
         self.inputname = tk.Entry(self.window, textvariable = meeting_name, width = 30, font = f2, )
+        self.lbldate = tk.Label(self.window, textvariable = self.text, font = f2, )  # 新增
         # self.lbltime = tk.Label(self.pageB, text = "會議日期：", height = 1, width = 50, font = f2)
         self.btnYes = tk.Button(self.window, text = "確認", height = 1, width = 10, font = f2, command = self.click_btnYes)
+        self.btndate = tk.Button(self.window, text = "新增日期", height = 1, width = 10, font = f2)  # 新增
         
         # self.lblTitle_B.place(x = 0, y = 50)
         self.lblname.place(x = 60, y = 75)
+        self.lblname1.place(x = 60, y = 150)  # 新增
+        
         # self.lbltime.place(x = 120, y = 220)
         self.inputname.place(x = 175, y = 75)
+        self.lbldate.place(x = 60, y = 180)  # 新增
         self.btnYes.place(relx = 0.5, y = 350, anchor = 'center')
-        datetime = calendar.datetime.datetime #日期和時間結合
+        self.btndate.place(x = 200,y=350,relx = 0.5, anchor = 'center')  # 新稱
+        datetime = calendar.datetime.datetime #日期和時間結合(從這邊複製)
         timedelta = calendar.datetime.timedelta  # 時間差
         class Calendar:
             def __init__(s, point = None, position = None):
                 # point    窗口位置
                 # position 窗口在點的位置 'ur'-右上, 'ul'-左上, 'll'-左下, 'lr'-右下
-                s.master = tk.Toplevel()  
                 fwday = calendar.SUNDAY 
 
-                year = datetime.now().year  # 打開頁面時為當下年月份
-                month = datetime.now().month
-                locale = None
-                sel_bg = '#ecffc4'  #選取框框色
-                sel_fg = '#05640e'  # 選取字底色
+                year = datetime.now().year  # 打開頁面時為當下年份
+                month = datetime.now().month  # 打開頁面時為當下月份
+                locale = None  # 地域設定
+                sel_bg = '#ecffc4'  #點擊後框框色
+                sel_fg = '#05640e'  # 點擊後字底色
 
                 s._date = datetime(year, month, 1)  # 該月份第一天
                 s._selection = None # 設置未選中的日期
 
-                s.G_Frame = ttk.Frame(s.master)
+                s.G_Frame = ttk.Frame(self.window)
 
                 s._cal = s.__get_calendar(locale, fwday)   # 實例化適當的日曆類
 
@@ -113,25 +121,13 @@ class PageA:
                 # 在當前空日曆中插入日期
                 s._update()
 
-                s.G_Frame.pack(expand = 1, fill = 'both')
-                s.master.overrideredirect(1)  # 調整字
-                s.master.update_idletasks()  # 刷新頁面
-                width, height = s.master.winfo_reqwidth(), s.master.winfo_reqheight()  # 寬高居中
-                if   position == 'ur':
-                    x, y = point[0], point[1] - height  
-                elif position == 'lr':
-                    x, y = point[0], point[1]
-                elif position == 'ul':
-                    x, y = point[0] - width, point[1] - height
-                elif position == 'll':
-                    x, y = point[0] - width, point[1]
-                else:
-                    x, y = (s.master.winfo_screenwidth() - width)/2, (s.master.winfo_screenheight() - height)/2
-                s.master.geometry('%dx%d+%d+%d' % (width, height, x, y)) #窗口位置居中
-                s.master.after(300, s._main_judge)  #每300秒更新一次
-                s.master.deiconify()  # 還原視窗 
-                s.master.focus_set()  # 焦點設置在所需小部件上
-                s.master.wait_window() 
+                s.G_Frame.pack(expand = 10)  
+                self.window.update_idletasks()  # 刷新頁面
+               
+                
+                self.window.deiconify()  # 還原視窗 
+                self.window.focus_set()  # 焦點設置在所需小部件上
+                self.window.wait_window()  # 直到按確定
                 
                 
             def __get_calendar(s, locale, fwday):  # 日曆文字化
@@ -141,14 +137,14 @@ class PageA:
                     return calendar.LocaleTextCalendar(fwday, locale)
                     
             def __setup_styles(s): # 自定義TTK風格
-                style = ttk.Style(s.master)
+                style = ttk.Style(self.window)
                 arrow_layout = lambda dir: ([('Button.focus', {'children': [('Button.%sarrow' % dir, None)]})])  # 返回參數性質
                 style.layout('L.TButton', arrow_layout('left'))  #製作點選上個月的箭頭
                 style.layout('R.TButton', arrow_layout('right'))  # 製作點選下個月的箭頭
                 
             
             def __place_widgets(s):  # 標題框架及其小部件
-                Input_judgment_num = s.master.register(s.Input_judgment)  # 需要将函数包装一下，必要的
+                Input_judgment_num = self.window.register(s.Input_judgment)  # 需要将函数包装一下，必要的
                 hframe = ttk.Frame(s.G_Frame)
                 gframe = ttk.Frame(s.G_Frame)
                 bframe = ttk.Frame(s.G_Frame)
@@ -175,8 +171,6 @@ class PageA:
                 # 日曆部件
                 s._calendar = ttk.Treeview(gframe, show='', selectmode='none', height=7)
                 s._calendar.pack(expand=1, fill='both', side='bottom', padx=5)
-                ttk.Button(bframe, text = "加入", width = 6, command = lambda: s._exit(True)).grid(row = 0, column = 0, sticky = 'ns', padx = 20)
-                ttk.Button(bframe, text = "取消", width = 6, command = s._exit).grid(row = 0, column = 1, sticky = 'ne', padx = 20)
                 tk.Frame(s.G_Frame, bg = '#565656').place(x = 0, y = 0, relx = 0, rely = 0, relwidth = 1, relheigh = 2/200)
                 tk.Frame(s.G_Frame, bg = '#565656').place(x = 0, y = 0, relx = 0, rely = 198/200, relwidth = 1, relheigh = 2/200)
                 tk.Frame(s.G_Frame, bg = '#565656').place(x = 0, y = 0, relx = 0, rely = 0, relwidth = 2/200, relheigh = 1)
@@ -240,7 +234,7 @@ class PageA:
                     return
                 bbox = widget.bbox(item, column)
                 if not bbox: # 日曆尚未出現
-                    s.master.after(20, lambda : s._pressed(item = item, column = column, widget = widget))
+                    self.window.after(20, lambda : s._pressed(item = item, column = column, widget = widget))
                     return
                 text = '%02d' % text
                 s._selection = (text, item, column)
@@ -260,7 +254,8 @@ class PageA:
                 s._selection = None
 
                 year, month = s._date.year, s._date.month
-                s._date = s._date + timedelta(days=calendar.monthrange(year, month)[1] + 1)
+                s._date = s._date + timedelta(
+                    days=calendar.monthrange(year, month)[1] + 1)
                 s._date = datetime(s._date.year, s._date.month, 1)
                 s.CB_year.set(s._date.year)
                 s.CB_month.set(s._date.month)
@@ -281,12 +276,9 @@ class PageA:
                         if day in day_list:
                             item = 'I00' + str(_item + 2)
                             column = '#' + str(day_list.index(day)+1)
-                            s.master.after(100, lambda :s._pressed(item = item, column = column, widget = s._calendar))
+                            self.window.after(100, lambda :s._pressed(item = item, column = column, widget = s._calendar))
 
-            def _exit(s, confirm = False):
-                if not confirm:
-                    s._selection = None
-                s.master.destroy()
+
 
             def _main_judge(s):
                 try:
@@ -295,12 +287,10 @@ class PageA:
                     #else: s.master.after(10, s._main_judge)
 
                     #s.master 為 toplevel 窗口
-                    if s.master.focus_displayof() == None or 'toplevel' not in str(s.master.focus_displayof()): s._exit()
-                    else: s.master.after(10, s._main_judge)
+                    if self.window.focus_displayof() == None or 'toplevel' not in str(self.window.focus_displayof()): s._exit()
+                    else: self.window.after(10, s._main_judge)
                 except:
-                    s.master.after(10, s._main_judge)
-
-                #s.master.tk_focusFollowsMouse() # 焦點跟随游標
+                    self.window.after(10, s._main_judge)
             def selection(s):
                 if not s._selection: 
                     return None
@@ -313,33 +303,13 @@ class PageA:
                     return True
                 else:
                     return False
+            
+   
+       
+        cal = Calendar()
 
-        if __name__ == '__main__':  
-            date_list =[]
-            date_str = tk.StringVar()
-            width = self.window.winfo_reqwidth() + 50
-            height = 100 #窗口大小
-            x, y = (self.window.winfo_screenwidth()  - width )/2, (self.window.winfo_screenheight() - height)/2
-            self.date = tk.Entry(self.window, textvariable = date_str,width = 30)  # 放選擇的日期
-            self.date.place(x = 175, y = 180)
-            date_str_gain = lambda: [date_str.set(date) for date in [Calendar((x, y), 'ur').selection()] if date]
-            def press_add():
-                date_list.append(str(date.get()))
-                for i in range(len(date_list)):
-                    date_label = tk.Label(self.window,text = "你已選擇"+date_list[i]+",")
-                date_label.place(x = 70, y = 90, relx = 1/20, rely = 1/6, relwidth = 4/8, relheigh = 2/30)
-                date.delete(0,"end")
-            def write():
-                press_add()
-                wb = openpyxl.load_workbook('C:/Users/sunny/Desktop/日期.xlsx')
-                sheet = wb['工作表1']
-                for i in range(len(date_list)):
-                    sheet.cell(row=1,column=i+2).value = date_list[i]
-                wb.save('C:/Users/sunny/Desktop/日期.xlsx')
-            tk.Button(self.window, text = '選擇日期:', command = date_str_gain).place(x = 60, y = 180)
-            tk.Button(self.window, text = "新增日期", command = press_add).place(x = 350, y = 320, relx = 1/20, rely = 1/6, relwidth = 4/80, relheigh = 2/30)
-            tk.Button(self.window, text = "確定",command = write).place(x = 450, y = 320, relx = 1/20, rely = 1/6, relwidth = 4/80, relheigh = 2/30)
-    
+ 
+      
     
     def click_btnYes(self):
         self.window.destroy()
@@ -354,11 +324,10 @@ class PageA:
         self.newMeeting = tk.Button(self.pageA, text = meeting_name.get(), height = 2, width = 10, font = f1)
         self.newMeeting.place(x = 50 + 325*int(self.count_meetings%3), y = 150 + 150*int(self.count_meetings/3))
         
-        data = openpyxl.load_workbook('紀錄會議日期時間.xlsx')
+        data = openpyxl.load_workbook('C:/Users/sunny/Desktop/日期.xlsx')
         sheet = data.create_sheet(meeting_name.get(),0)  # 將工作表命名為會議名稱
         sheet.cell(row = 1, column = 2).value = meeting_name.get()
-        self.newMeeting = tk.Button(self.pageA, text = meeting_name.get(), height = 2, width = 10, font = f1).place(x = 50, y = 150)
-        data.save('紀錄會議日期時間.xlsx')
+        data.save('C:/Users/sunny/Desktop/日期.xlsx')
 
 
 root.geometry("1000x700")
